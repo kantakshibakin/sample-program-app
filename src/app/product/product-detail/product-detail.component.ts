066832494d55f7e272fdd8c142c19d21b0b42d05
kan <t.kanako14@gmail.com>
2021-12-13 16:46:52 +0900

    フロントエンドとバックエンドの結合

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from 'src/app/products';
+import { ProductService } from '../shared/product.service';
 
 
 @Component({
@@ -12,12 +13,25 @@ export class ProductDetailComponent implements OnInit {
   product:any;
   // product: any = {}
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(
+    private route:ActivatedRoute,
+    private productService: ProductService
+    ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe (params => {
-        this.product = products[+params.get('productId')!]
-        console.log('productId');
+        // this.product = this.productService.getProductById(params.get('productId')!)      
+        // console.log('productId');
+
+        const productObsarvable = this.productService.getProductById(params.get('productId')!)      
+        productObsarvable.subscribe (
+          (data) => {
+          this.product = data
+          },
+          (err) => {
+            
+          }
+        )
     })
 
   }
