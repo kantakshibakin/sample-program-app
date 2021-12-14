fa2fbc64eca8b9038041929635222d17c5da7989
kan <t.kanako14@gmail.com>
2021-12-14 17:46:19 +0900

    Hwroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products') 
 // await mongoose.connect('mongodb+srv://test:testtest@cluster0.yevcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
     useNewUrlParser:true,
     useUnifiedTopology:true
     }).then (
         () => {
-            const fakeDb = new FakeDb()
-            fakeDb.initDb()
+            if (process.env.NODE_ENV !== 'production') {
+                const fakeDb = new FakeDb()
+                //（必要に応じて）データベースの初期化を行う
+                // fakeDb.initDb()
+            }
         }
     )
 
@@ -21,9 +25,17 @@ const app = express()
 
 app.use('/api/v1/products', productRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+    const appPath = path.join( __dirname, '..', 'dist', 'sample-program-app')
+        app.use(express.static(appPath))
+        app.get("*", function(req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'))
+    })
+}
 // app.get('/products', function(req, res){
 //     res.json({'success' : true})
 // })
+
 
 /*PORTが読めないときは　3001 とする */
 const PORT = process.env.PORT || '3001'
